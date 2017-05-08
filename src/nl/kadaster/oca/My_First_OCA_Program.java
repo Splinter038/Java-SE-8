aa51ea488e7623943f3d2182804115694806cba2
Splinter038 <mbbreet@gmail.com>
2017-05-08 15:58:31 +0200

    2e poging

--- src/nl/kadaster/oca/My_First_OCA_Program.java
+++ src/nl/kadaster/oca/My_First_OCA_Program.java
@@ -5,6 +5,7 @@ public class My_First_OCA_Program {
 	public static void main(String[] args) {
 		System.out.println("OCA");
 		System.out.println("toevoeging");
+		System.out.println("toevoeging2");
 	}
 
 }
