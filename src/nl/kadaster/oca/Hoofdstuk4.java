d51412d7c423bbdc304cd5995ed1c6fa4dd17f82
Splinter038 <mbbreet@gmail.com>
2017-05-17 16:13:01 +0200

    opruiming

--- src/nl/kadaster/oca/Hoofdstuk4.java
+++
@@ -1,9 +0,0 @@
-package nl.kadaster.oca;
-
-public class Hoofdstuk4 {
-	public static void main(String ... blabla){
-		String test = "Hallo";
-		System.out.println(test.replace("ll", "LL"));
-		String a;
-	}
-}
